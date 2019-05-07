6b07bcc49030ebea94bf08866ddafbf59688c70b
bakinovsky-m <makbak@ya.ru>
2019-05-07 22:42:22 +0300

    som: images name

--- som.py
+++ som.py
@@ -159,17 +159,17 @@ while running:
         draw_som(screen, s)
         pygame.display.update()
     if counter == 0:
-        pygame.image.save(screen, "0_iter.png")
+        pygame.image.save(screen, "som_0_iter.png")
     if counter == 100:
-        pygame.image.save(screen, "100_iter.png")
+        pygame.image.save(screen, "som_100_iter.png")
     if counter == 1000:
-        pygame.image.save(screen, "1000_iter.png")
+        pygame.image.save(screen, "som_1000_iter.png")
     if counter == 5000:
-        pygame.image.save(screen, "5000_iter.png")
+        pygame.image.save(screen, "som_5000_iter.png")
     if counter == 10000:
-        pygame.image.save(screen, "10000_iter.png")
+        pygame.image.save(screen, "som_10000_iter.png")
     if counter == 15000:
-        pygame.image.save(screen, "15000_iter.png")
+        pygame.image.save(screen, "som_15000_iter.png")
     # start = time.time()
     s.train(dataset)
     # end = time.time()
